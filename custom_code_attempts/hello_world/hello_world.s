fce245cae50b929f7042a6ca260536ea3a379ee6
Ryan Gambrell <ryanpeytongambrell@gmail.com>
2025-05-11 23:33:23 +0000

    Modified Hello World source file to print the entire message (using jump and lodsb instructions).

--- custom_code_attempts/hello_world/hello_world.s
+++ custom_code_attempts/hello_world/hello_world.s
@@ -3,9 +3,18 @@
 .global init
 
 init: 
-    mov $0x0e41, %ax # set AH register to 0xe (function teletype) and AL register to 0x41 (ASCII "A") 
-    int $0x10 # call function in AH from interrupt 0x10
+    mov $msg, %si # loads the first address of msg into si
+    mov $0x0e, %ah # sets AH register to 0xe (function teletype)
+print_char:
+    lodsb # loads the byte from the address (in si) into al and increments si
+    cmp $0, %al # compares content in AL with zero
+    je done # if al == 0, go to "done"
+    int $0x10 # prints next character in AL register to screen
+    jmp print_char # repeat with next byte
+done:
     hlt # Stops executing
+
+msg: .asciz "Hello world!" # stores the string (plus a byte with value "0") and gives access via the $msg label
 
 .fill 510-(.-init), 1, 0 # add zeros to make it 510 bytes long
 
