4cf62e73587acf6dfd3da9c6843157269d9e9437
Ryan Gambrell <ryanpeytongambrell@gmail.com>
2025-05-11 23:35:30 +0000

    Execution Shell Script Modification: Add QEMU (for testing code as bare-metal bootloader) and modified linker parameters to generate a strict binary file (and to load at address 0x7c00) to go along with Medium tutorial (https://medium.com/@g33konaut/writing-an-x86-hello-world-boot-loader-with-assembly-3e4c5bdd96cf).

--- custom_code_attempts/hello_world/run.sh
+++ custom_code_attempts/hello_world/run.sh
@@ -4,4 +4,7 @@
 as -o hello_world.o --32 hello_world.s
 
 # Link File
-ld -o hello_world.bin -e init -m elf_i386 hello_world.o
+ld -o hello_world.bin --oformat binary -e init -Ttext 0x7c00 -m elf_i386 hello_world.o
+
+# Run QEMU Emulation (For Testing) (Control A + X to terminate)
+qemu-system-i386 -nographic hello_world.bin 
